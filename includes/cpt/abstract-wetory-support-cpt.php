641273914266cd7be0c92898100f17b90bdb859f
Tomáš Rybnický <tomas.rybnicky@wetory.eu>
2020-10-31 00:00:00 +0100

    Notice dashboard post type + fixes in abstract cpt class Task #2 - CPT-0001

--- includes/cpt/abstract-wetory-support-cpt.php
+++ includes/cpt/abstract-wetory-support-cpt.php
@@ -15,7 +15,7 @@
 use Wetory_Support_Options as Plugin_Options;
 
 abstract class Wetory_Support_Cpt {
-    
+
     use Wetory_Support_Object_File;
 
     /**
@@ -36,7 +36,7 @@ abstract class Wetory_Support_Cpt {
     public function __construct(string $id) {
         $this->id = sanitize_key($id);
     }
-    
+
     /**
      * Prepare arguments for registering post type. Implementation is always sub-class specific.
      * @since    1.1.0
@@ -51,10 +51,114 @@ abstract class Wetory_Support_Cpt {
      * 
      * @since    1.1.0
      */
-    public function register(){
-        if ($this->use_cpt()) {
-            register_post_type($this->id, $this->get_arguments());
+    public function register() {
+        //if ($this->use_cpt()) {
+         register_post_type($this->id, $this->get_arguments());
+//        $this->unregister();
+        //}
+    }
+
+    /**
+     * Unregistering custom post type.
+     * 
+     * https://developer.wordpress.org/reference/functions/unregister_post_type/
+     * 
+     * @see Wetory_Support_Cpt::database_cleanup()
+     * @since 1.1.0
+     */
+    public function unregister() {
+        unregister_post_type($this->id);
+        $this->database_cleanup();
+    }
+
+    /**
+     * Remove custom post type posts from database.
+     *  
+     * https://developer.wordpress.org/reference/classes/wpdb/
+     * https://developer.wordpress.org/reference/functions/wp_delete_post/
+     * 
+     * @since 1.1.0     * 
+     * 
+     * @global wpdb $wpdb WordPress database access abstraction class.
+     */
+    public function database_cleanup() {
+        global $wpdb;
+
+        $posts = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type = '" . $this->id . "'");
+        foreach ((array) $posts as $post) {
+            wp_delete_post($post, true);
         }
+        
+        wetory_write_log('Database cleanup performed for custo post type "'.$this->id.'", removed '.sizeof($posts).' posts.', 'info');
+    }
+
+    /**
+     * Default labels can be changed - need to be overridden with calling parent
+     * 
+     * Just optional function which can be overridden in subclass to change default labels
+     * 
+     * @since    1.1.0
+     * @return array An array of labels for this post type. 
+     */
+    protected function labels() {
+        $labels = array(
+            'name'                  => _x('Post Types', 'Post Type General Name', 'wetory-support'),
+            'singular_name'         => _x('Post Type', 'Post Type Singular Name', 'wetory-support'),
+            'menu_name'             => __('Post Types', 'wetory-support'),
+            'name_admin_bar'        => __('Post Type', 'wetory-support'),
+            'archives'              => __('Item Archives', 'wetory-support'),
+            'attributes'            => __('Item Attributes', 'wetory-support'),
+            'parent_item_colon'     => __('Parent Item:', 'wetory-support'),
+            'all_items'             => __('All Items', 'wetory-support'),
+            'add_new_item'          => __('Add New Item', 'wetory-support'),
+            'add_new'               => __('Add New', 'wetory-support'),
+            'new_item'              => __('New Item', 'wetory-support'),
+            'edit_item'             => __('Edit Item', 'wetory-support'),
+            'update_item'           => __('Update Item', 'wetory-support'),
+            'view_item'             => __('View Item', 'wetory-support'),
+            'view_items'            => __('View Items', 'wetory-support'),
+            'search_items'          => __('Search Item', 'wetory-support'),
+            'not_found'             => __('Not found', 'wetory-support'),
+            'not_found_in_trash'    => __('Not found in Trash', 'wetory-support'),
+            'featured_image'        => __('Featured Image', 'wetory-support'),
+            'set_featured_image'    => __('Set featured image', 'wetory-support'),
+            'remove_featured_image' => __('Remove featured image', 'wetory-support'),
+            'use_featured_image'    => __('Use as featured image', 'wetory-support'),
+            'insert_into_item'      => __('Insert into item', 'wetory-support'),
+            'uploaded_to_this_item' => __('Uploaded to this item', 'wetory-support'),
+            'items_list'            => __('Items list', 'wetory-support'),
+            'items_list_navigation' => __('Items list navigation', 'wetory-support'),
+            'filter_items_list'     => __('Filter items list', 'wetory-support'),
+        );
+        
+        return array_replace($labels, $this->override_labels());
+    }
+    
+    /**
+     * Change default labels - need to be overridden
+     * 
+     * Just optional function which can be overridden in subclass to change default
+     * post type labels.
+     * 
+     * @since 1.1.0
+     * @return array An array of labels for this post type. 
+     */
+    protected function override_labels(){
+        return array();
+    }
+
+    /**
+     * Specify rewrite rules for custom permalinks
+     * 
+     * This can be overridden in sub-class. To specify rewrite rules, an array 
+     * can be passed - see register_post_type documentation section 'rewrite'
+     * https://developer.wordpress.org/reference/functions/register_post_type/#parameters
+     * 
+     * @since 1.1.0
+     * @return boolean Defaults to true, using $post_type as slug
+     */
+    protected function rewrite() {
+        return true;
     }
 
     /**
@@ -89,10 +193,10 @@ abstract class Wetory_Support_Cpt {
      */
     public function get_options() {
         $options = Plugin_Options::get_cpt_options($this->id);
-        
+
         return $options;
     }
-    
+
     /**
      * Check if custom post type is configured for use. 
      * 
@@ -100,7 +204,7 @@ abstract class Wetory_Support_Cpt {
      * @see Plugin_Options::use_cpt($cpt)
      * @return boolean
      */
-    public function use_cpt(){
+    public function use_cpt() {
         return Plugin_Options::use_cpt($this->id);
     }
 
--- includes/cpt/cpt-wetory-support-notice-board.php
+++ includes/cpt/cpt-wetory-support-notice-board.php
@@ -31,9 +31,47 @@ class Cpt_Wetory_Support_Notice_Board extends Wetory_Support_Cpt {
      * @return array Arguments for registering post type
      */
     protected function get_arguments() {
-        $args = array();
+        $args = array(
+            'label'                 => __('Notice board', 'text_domain'),
+            'description'           => __('Custom post type notice board is used for official records which is mandatory for city/village websites.', 'text_domain' ),
+            'labels'                => $this->labels(),
+            'supports'              => array('title', 'editor', 'revisions', 'custom-fields'),
+            'taxonomies'            => array('category', 'post_tag'),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'menu_icon'             => 'dashicons-clipboard',
+            'show_in_admin_bar'     => false,
+            'show_in_nav_menus'     => true,
+            'can_export'            => false,
+            'has_archive'           => false,
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'show_in_rest'          => true,
+            'rewrite'               => $this->rewrite(),
+            'capability_type'       => 'page', 
+        );
         
         return $args;
+    }
+    
+    /**
+     * Overriding default labels from parent class
+     * 
+     * @since    1.1.0
+     *    
+     * @return array An array of labels for this post type. 
+     */
+    protected function override_labels() {
+        $overriden_labels = array(
+            'name'                  => _x( 'Notice board', 'Post Type General Name', 'wetory-support' ),
+            'singular_name'         => _x( 'Notice board post', 'Post Type Singular Name', 'wetory-support' ),
+            'menu_name'             => __( 'Notice board', 'wetory-support' ),
+            'name_admin_bar'        => __( 'Notice board', 'wetory-support' ),
+        );
+        return $overriden_labels;
     }
 
 }
