828b2e86d135e70f8dbc80755afa481fd03392d6
Tomáš Rybnický <tomas.rybnicky@wetory.eu>
2023-02-13 19:48:17 +0100

    New section with links introduced
    Signed-off-by: Tomáš Rybnický <tomas.rybnicky@wetory.eu>

--- admin/views/admin-settings-connections.php
+++ admin/views/admin-settings-connections.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Provide a admin area view for the plugin settings
  *
@@ -19,15 +20,39 @@ if (!defined('WPINC')) {
 
 <div class="wetory-support-tab-content" data-id="<?php echo esc_attr($target_id); ?>">
     <ul class="wetory-support-sub-tab">
-        <li data-target="apikeys"><a><?php echo esc_html__('API Keys', 'wetory-support'); ?></a></li>   
-    
+        <li data-target="apikeys"><a><?php echo esc_html__('API Keys', 'wetory-support'); ?></a></li>
+        <li data-target="links"><a><?php echo esc_html__('Links', 'wetory-support'); ?></a></li>
     </ul>
     <div class="wetory-support-sub-tab-container">
         <div class="wetory-support-sub-tab-content" data-id="apikeys" style="display:block;">
-            <?php do_action('wetory_settings_render_section','apikeys'); ?>
+            <?php do_action('wetory_settings_render_section', 'apikeys'); ?>
+        </div>
+        <div class="wetory-support-sub-tab-content" data-id="links" style="display:block;">
+            <div class="wetory-support-settings-section links">
+                <h3><?php echo esc_html__('Links', 'wetory-support'); ?></h3>
+                <p><?php _e('Usefull links to hidden plugins and external tools.', 'wetory-support'); ?></p>
+                <table class="form-table">
+                    <?php if (is_plugin_active('backwpup/backwpup.php') || true) : ?>
+                        <tr valign="top">
+                            <th scope="row"><?php _e('BackWPup', 'wetory-support'); ?></th>
+                            <td>
+                                <a href="wp-admin/admin.php?page=backwpupjobs"><?php _e('Jobs', 'wetory-support'); ?></a> | 
+                                <a href="wp-admin/admin.php?page=backwpuplogs"><?php _e('Logs', 'wetory-support'); ?></a> | 
+                                <a href="https://wordpress.org/plugins/backwpup/" target="_blank" title="<?php _e('Go to plugin page', 'wetory-support'); ?>"><?php _e('Plugin page', 'wetory-support'); ?></a>
+                            </td>
+                        </tr>
+                    <?php endif; ?>
+                    <tr valign="top">
+                        <th scope="row"><?php _e('Wetory', 'wetory-support'); ?></th>
+                        <td>
+                            <a href="https://wm.z-wetory.eu/" target="_blank" title="<?php _e('Go to page', 'wetory-support'); ?>"><?php _e('Wordpress Manager', 'wetory-support'); ?></a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
     <?php
-	require 'admin-settings-button-save.php';
-	?>
+    require 'admin-settings-button-save.php';
+    ?>
 </div>
